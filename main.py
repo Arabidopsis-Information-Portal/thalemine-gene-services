d67f62c1ea959a11a1a807e85782d49984e8f20b
Chezhian <ichezhia@ichezhia-osx.jcvi.org>
2015-06-23 15:03:44 -0400

    hello

--- main.py
+++ main.py
@@ -2,7 +2,29 @@
 
 import json
 
+#The genes will be indexed with Gene ID
+
+
+#ID Search
+
+
 def search(args):
     print json.dumps({'obj': 1, 'args': args})
     print "--- ++"
     print json.dumps({'obj': 2, 'args': args})
+
+def search(args):
+
+
+
+
+def search(args):
+
+
+
+
+def search(args):
+
+
+
+#Keyword Search
