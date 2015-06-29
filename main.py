8d123eef3593d9e59050fe750c466aadcd8f82eb
Chezhian <ichezhia@ichezhia-osx.jcvi.org>
2015-06-29 16:30:03 -0400

    code cleanup general and intermine code cleanup

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 from intermine.webservice import Service
 
 service = Service("https://apps.araport.org:443/thalemine/service")
+query = service.new_query("Gene")
+query.add_view(
+    "briefDescription", "computationalDescription", "curatorSummary", "name",
+    "length", "primaryIdentifier", "secondaryIdentifier", "symbol"
+)
 
 #function used to test code
 #python -c 'import main; print main.function("<parameter>")'
@@ -17,9 +22,6 @@ def function(arg):
 def returnList():
     geneIDList = []
 
-    query = service.new_query("Gene")
-    query.add_view("primaryIdentifier")
-
     for row in query.rows():
         geneIDList.append(str(row["primaryIdentifier"]))
 
@@ -28,10 +30,6 @@ def returnList():
 #recieves a GeneID - returns briefDescription, easily changeable to any field desired
 def returnInfo(id):
 #this function will look at the geneID from the search and return the basic info of that geneID
-
-    query = service.new_query("Gene")
-    query.add_view("primaryIdentifier", "briefDescription")
-
     print "Brief Description of %s:" %id
 
     for row in query.rows():
@@ -52,38 +50,5 @@ def wildcardGeneID(arg):
     for row in query.rows():
         if arg in str(row["primaryIdentifier"]):
             geneIDList.append(row["primaryIdentifier"])
-            
+
     return geneIDList
-
-#ignore below code
-"""
-#TODO
-#Keyword Search - like Google
-def searchKeyword(arg):
-
-    results = []
-
-    text = str(arg)
-
-    #default value if there is no match
-    geneID = "0"
-
-    query = service.new_query("Gene")
-
-    # The view specifies the output columns
-    query.add_view(
-        "briefDescription", "symbol", "secondaryIdentifier", "primaryIdentifier",
-        "name", "length", "curatorSummary", "computationalDescription"
-    )
-
-    for row in query.rows():
-        if (text in row["briefDescription"]) or (text in row["symbol"])                 \
-        or (text in row["secondaryIdentifier"]) or (text in row["primaryIdentifier"])   \
-        or (text in row["name"]) or (text in row["curatorSummary"])                     \
-        or (text in row["computationalDescription"]):
-            results.extend(row["primaryIdentifier"])
-
-    #should return an array of geneID
-    return geneID
-"""
-#return json.dumps(variable)
