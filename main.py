223afb7c9a3a7e8defb78923871e92a01efed70b
Chezhian <ichezhia@ichezhia-osx.jcvi.org>
2015-06-30 16:26:47 -0400

    cleanup

--- main.py
+++ main.py
@@ -30,11 +30,7 @@ def returnList():
 
     return geneIDList
 
-#recieves a GeneID - returns briefDescription, easily changeable to any field desired
 def returnInfo(id, out):
-#this function will look at the geneID from the search and return the basic info of that geneID
-    #print "Brief Description of %s:" %id
-
     for row in query.rows():
         if id == row["primaryIdentifier"]:
             return row[out]
