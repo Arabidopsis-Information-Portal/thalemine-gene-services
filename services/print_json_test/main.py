a384c634a6ed1c4eb899c7e2f9f3a30fc991c7b4
Chezhian <ichezhia@ichezhia-osx.jcvi.org>
2015-07-16 17:06:12 -0400

    testing files

---
+++ services/print_json_test/main.py
@@ -0,0 +1,15 @@
+import json
+
+def search():
+    print json.dumps({'Name': 'Zara', 'Age': 7, 'Class': 'First'})
+    print '---'
+
+#operation
+def list():
+    print json.dumps({"primaryIdentifier": "AT12345",
+                "results" :
+                    {"chromosomeLocation.end" : "12",
+                        "chromosomeLocation.start" : 34}
+        })
+
+    print '---'
---
+++ services/print_rows_test/main.py
@@ -0,0 +1,50 @@
+import json
+
+#intermine is data service used by thalemine
+#generated python code for query
+from intermine.webservice import Service
+service = Service("https://apps.araport.org/thalemine/service")
+query = service.new_query("Gene")
+
+#adding views to the query
+query.add_view("primaryIdentifier", "chromosomeLocation.end", "chromosomeLocation.start")
+
+#operation
+def search(parameter):
+    #store input as variables
+    searchInput = parameter["Identifier"]
+    prefOutInput = parameter["Output"]
+
+    #determine what to return
+    if prefOutInput is 'All':
+        print json.dumps(returnAllInfo(searchInput))
+    else:
+        print json.dumps(returnInfo(searchInput,prefOutInput))
+
+#operation
+def list():
+    for row in query.rows():
+        print json.dumps({"primaryIdentifier" : row["primaryIdentifier"]})
+        print '---'
+
+#returns information for all fields
+def returnAllInfo(id):
+    #query search thalemine
+    query.add_constraint("primaryIdentifier", "=", id, code = "A")
+
+    #return dict of information of matching geneID
+    for row in query.rows():
+        return {"primaryIdentifier": row["primaryIdentifier"],
+                "results" :
+                    {"chromosomeLocation.end" : row["chromosomeLocation.end"],
+                        "chromosomeLocation.start" : row["chromosomeLocation.start"]
+                    }
+        }
+#returns specific info about specific geneID
+def returnInfo(id, out):
+    #query search thalemine
+    query.add_constraint("primaryIdentifier", "=", str(id), code = "A")
+
+    #return dict of information of matching geneID
+    for row in query.rows():
+        return {"primaryIdentifier": row["primaryIdentifier"], out : row[out]}
