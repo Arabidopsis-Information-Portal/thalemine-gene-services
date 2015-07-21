93f560d8e7a727c3538fe1990223509d4faa0a0e
Chezhian <ichezhia@ichezhia-osx.jcvi.org>
2015-07-21 09:57:29 -0400

    test

--- services/gene_by_geneid/main.py
+++ services/gene_by_geneid/main.py
@@ -1,9 +1,9 @@
 import json
-
+#service to access thalemine database
 from intermine.webservice import Service
 service = Service("https://apps.araport.org/thalemine/service")
 
-#operation
+#operation to print the last ~20 locus ids
 def search(parameter):
 
     querySearch = service.new_query("Gene")
@@ -18,7 +18,7 @@ def search(parameter):
         cnt += 1
     return
 
-#operation
+#operation to print the first 30000 locus ids
 def list(parameter):
 
     querySearch = service.new_query("Gene")
@@ -32,5 +32,4 @@ def list(parameter):
         cnt += 1
         if (cnt > 30000):
             break
-
     return
