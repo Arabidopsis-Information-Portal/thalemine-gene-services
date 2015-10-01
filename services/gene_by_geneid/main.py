c924557cd8e0a6fd62110a6701a56945c6b27c54
eriksf <erik@jcvi.org>
2015-10-01 19:08:14 -0400

    [ED-116] added provenance and input validation to metadata cleaned up code

--- services/gene_by_geneid/main.py
+++ services/gene_by_geneid/main.py
@@ -10,15 +10,32 @@ service = Service("https://apps.araport.org/thalemine/service")
 #this search operation is supported by the Adama middleware on Araport
 #endpoints of this name are to return certain data of a gene search
 def search(parameter):
-    #store input as variables
+    # store input as variables
     searchInput = parameter["Identifier"]
     prefOutInput = parameter["Output"]
 
-    #determine what to return
-    if prefOutInput == "all":
-        print json.dumps(returnAllInfo(searchInput))
-    else:
-        print json.dumps(returnInfo(searchInput,prefOutInput))
+    # auto-generated query code
+    # query search thalemine
+    query = service.new_query("Gene")
+    # adding views to the query
+    query.add_view("primaryIdentifier", "chromosomeLocation.end", "chromosomeLocation.start")
+    query.add_constraint("primaryIdentifier", "=", searchInput, code = "A")
+
+    # print dict of information of matching geneID
+    for row in query.rows():
+        if prefOutInput == "all":
+            record = {
+                "locus_id": row["primaryIdentifier"],
+                "chromosomeLocation.end": row["chromosomeLocation.end"],
+                "chromosomeLocation.start": row["chromosomeLocation.start"]
+            }
+        else:
+            record = {
+                "locus_id": row["primaryIdentifier"],
+                prefOutInput: row[prefOutInput]
+            }
+        print json.dumps(record, indent=2)
+        print '---'
 
 #function that responds when the URL ends in /list
 #this list operation is supported by the Adama middleware on Araport
@@ -29,38 +46,8 @@ def list(parameter):
     queryList.add_view("primaryIdentifier", "chromosomeLocation.end", "chromosomeLocation.start")
 
     for row in queryList.rows():
-        org = {}
-        org['locus_id'] = row["primaryIdentifier"]
-        print json.dumps(org)
+        org = {
+            'locus_id': row['primaryIdentifier']
+        }
+        print json.dumps(org, indent=2)
         print '---'
-    return
-
-#returns information for all fields for specfic geneID
-def returnAllInfo(id):
-    #auto-generated query code
-    #query search thalemine
-    query = service.new_query("Gene")
-    #adding views to the query
-    query.add_view("primaryIdentifier", "chromosomeLocation.end", "chromosomeLocation.start")
-    #manipulate the auto-generated code to constrain to a variable
-    query.add_constraint("primaryIdentifier", "=", id, code = "A")
-
-    #return dict of information of matching geneID
-    for row in query.rows():
-        return {"locus_id": row["primaryIdentifier"],
-                "chromosomeLocation.end" : row["chromosomeLocation.end"],
-                "chromosomeLocation.start" : row["chromosomeLocation.start"]
-                }
-
-#returns specific info about specific geneID
-def returnInfo(id, out):
-    #auto-generated query code
-    #query search thalemine
-    query = service.new_query("Gene")
-    #adding views to the query
-    query.add_view("primaryIdentifier", "chromosomeLocation.end", "chromosomeLocation.start")
-    query.add_constraint("primaryIdentifier", "=", id, code = "A")
-
-    #return dict of information of matching geneID
-    for row in query.rows():
-        return {"locus_id": row["primaryIdentifier"], out : row[out]}
--- services/go_by_geneid/main.py
+++ services/go_by_geneid/main.py
@@ -5,6 +5,13 @@ import json
 #auto-generated service code
 from intermine.webservice import Service
 service = Service("https://apps.araport.org/thalemine/service")
+GO_FIELDS_MAP = {
+    'locus_id': 'goAnnotation.subject.primaryIdentifier',
+    'name': 'goAnnotation.ontologyTerm.name',
+    'def': 'goAnnotation.ontologyTerm.description',
+    'namespace': 'goAnnotation.ontologyTerm.namespace',
+    'evidence_code': 'goAnnotation.evidence.code.code'
+}
 
 #operation
 def search(parameter):
@@ -12,74 +19,47 @@ def search(parameter):
     searchInput = parameter["Identifier"]
     prefOutInput = parameter["Output"]
 
-    #determine what to return
-    if prefOutInput == "all":
-        returnAllInfo(searchInput)
-    else:
-        returnInfo(searchInput,prefOutInput)
+    # auto-generated query code
+    # query search thalemine
+    query = service.new_query("Gene")
+    # adding views to the query
+    query.add_view(
+        GO_FIELDS_MAP['locus_id'],
+        GO_FIELDS_MAP['name'],
+        GO_FIELDS_MAP['def'],
+        GO_FIELDS_MAP['namespace'],
+        GO_FIELDS_MAP['evidence_code']
+    )
+    # manipulate the auto-generated code to constrain to a variable
+    query.add_constraint(GO_FIELDS_MAP['locus_id'], "=", searchInput, code = "B")
 
-#operation
+    # print dict of information of matching geneID
+    for row in query.rows():
+        if prefOutInput == "all":
+            record = {
+                'locus_id' : row[GO_FIELDS_MAP['locus_id']],
+                'name' : row[GO_FIELDS_MAP['name']],
+                'def': row[GO_FIELDS_MAP['def']],
+                'namespace' : row[GO_FIELDS_MAP['namespace']],
+                'evidence_code' : row[GO_FIELDS_MAP['evidence_code']]
+            }
+        else:
+            gKey = [key for key, value in GO_FIELDS_MAP.iteritems() if value == prefOutInput][0]
+            record = {
+                'locus_id': row[GO_FIELDS_MAP['locus_id']],
+                gKey : row[prefOutInput]
+            }
+        print json.dumps(record, indent=2)
+        print '---'
+
 def list(parameter):
     #auto-generated query code
     queryList = service.new_query("Gene")
     queryList.add_view("primaryIdentifier", "chromosomeLocation.end", "chromosomeLocation.start")
 
     for row in queryList.rows():
-        org = {}
-        org['locus_id'] = row["primaryIdentifier"]
-        print json.dumps(org)
-        print '---'
-    return
-
-#returns information for all fields for specfic geneID
-def returnAllInfo(id):
-    #auto-generated query code
-    #query search thalemine
-    query = service.new_query("Gene")
-    #adding views to the query
-    query.add_view(
-    "goAnnotation.subject.primaryIdentifier",
-    "goAnnotation.ontologyTerm.name",
-    "goAnnotation.ontologyTerm.description",
-    "goAnnotation.ontologyTerm.namespace",
-    "goAnnotation.evidence.code.code"
-    )
-    #manipulate the auto-generated code to constrain to a variable
-    query.add_constraint("goAnnotation.subject.primaryIdentifier", "=", id, code = "B")
-
-
-    #return dict of information of matching geneID
-    for row in query.rows():
-        print json.dumps(
-                {"locus_id" : row["goAnnotation.subject.primaryIdentifier"],
-                "name" : row["goAnnotation.ontologyTerm.name"],
-                "def" : row["goAnnotation.ontologyTerm.description"],
-                "namespace" : row["goAnnotation.ontologyTerm.namespace"],
-                "evidence_code" : row["goAnnotation.evidence.code.code"],
-                }
-        )
-        print '---'
-
-#returns specific info about specific geneID
-def returnInfo(id, out):
-    #auto-generated query code
-    #query search thalemine
-    query = service.new_query("Gene")
-    #adding views to the query
-    query.add_view(
-    "goAnnotation.subject.primaryIdentifier",
-    "goAnnotation.ontologyTerm.name",
-    "goAnnotation.ontologyTerm.description",
-    "goAnnotation.ontologyTerm.namespace",
-    "goAnnotation.evidence.code.code"
-    )
-    #manipulate the auto-generated code to constrain to a variable
-    query.add_constraint("goAnnotation.subject.primaryIdentifier", "=", id, code = "B")
-
-    #return dict of information of matching geneID
-    for row in query.rows():
-        print json.dumps(
-                {"goAnnotation.subject.primaryIdentifier": row["goAnnotation.subject.primaryIdentifier"],
-                out : row[out]}
-        )
+        org = {
+            'locus_id': row['primaryIdentifier']
+        }
+        print json.dumps(org, indent=2)
         print '---'
