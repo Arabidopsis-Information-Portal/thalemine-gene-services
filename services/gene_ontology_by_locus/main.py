85da2571311614f85aa47d64cb0f8dfd21634676
eriksf <erik@jcvi.org>
2015-10-27 16:09:55 -0400

    now grouping evidence codes

--- services/gene_ontology_by_locus/main.py
+++ services/gene_ontology_by_locus/main.py
@@ -26,17 +26,40 @@ def search(args):
     query.add_constraint("primaryIdentifier", "=", locus, code = "A")
 
     # print dict of information of matching geneID
+    records = {}
     for row in query.rows():
+        gene_id = row["primaryIdentifier"]
+        description = row["goAnnotation.ontologyTerm.description"]
+        name = row["goAnnotation.ontologyTerm.name"]
+        namespace = row["goAnnotation.ontologyTerm.namespace"]
+        code = row["goAnnotation.evidence.code.code"]
+        dataset_name = row["goAnnotation.dataSets.name"]
+        dataset_version = row["goAnnotation.dataSets.version"]
+        key = "%s~~%s~~%s~~%s~~%s" % (name, description, namespace, dataset_name, dataset_version)
+        if not records.has_key(key):
+            records[key] = {
+                'locus': gene_id,
+                'description': description,
+                'name': name,
+                'namespace': namespace,
+                'evidence_codes': [],
+                'dataset_name': dataset_name,
+                'dataset_version': dataset_version
+            }
+        if code:
+            records[key]['evidence_codes'].append(code)
+
+    for r in records.values():
         record = {
             'class': 'locus_property',
             'source_text_description': 'ThaleMine GO record',
-            'locus': row["primaryIdentifier"],
-            'description': row["goAnnotation.ontologyTerm.description"],
-            'name': row["goAnnotation.ontologyTerm.name"],
-            'namespace': row["goAnnotation.ontologyTerm.namespace"],
-            'evidence_code': row["goAnnotation.evidence.code.code"],
-            'dataset_name': row["goAnnotation.dataSets.name"],
-            'dataset_version': row["goAnnotation.dataSets.version"]
+            'locus': r['locus'],
+            'description': r['description'],
+            'name': r['name'],
+            'namespace': r['namespace'],
+            'evidence_codes': r['evidence_codes'],
+            'dataset_name': r['dataset_name'],
+            'dataset_version': r['dataset_version']
         }
         print json.dumps(record, indent=2)
         print '---'
